428610e699659d66c00a0f669df613a09e20e0b8
ThienTho123 <thientho7a4@gmail.com>
2024-04-05 17:09:50 +0700

    Create Project

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "QuanLyTapHoa_Mobile"
+include(":app")
