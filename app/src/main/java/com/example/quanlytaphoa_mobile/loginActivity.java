71f1649de73d42719bf831408c57604a525fb175
NhiPhan-0201 <pnhi30121996@gmail.com>
2024-04-08 13:44:52 +0700

    giao dien dang ky, dang nhap, quan ly nhan vien

---
+++ app/src/main/java/com/example/quanlytaphoa_mobile/loginActivity.java
@@ -0,0 +1,13 @@
+package com.example.quanlytaphoa_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class loginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
